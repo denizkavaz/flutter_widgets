44f1d950568ce00fa0830cc29cb1b0cbbf96e669
deniz.kavaz <106762076+denizkavaz@users.noreply.github.com>
2023-12-21 18:04:37 +0300

    Create color.dart

---
+++ color.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+// Brand Colors
+const kPrimaryColor = Color(0xff092C4C);
+const kSecondaryColor = Color(0xffF2994A);
+// State Colors
+const kInfoColor = Color(0xff2F80ED);
+const kSuccessColor = Color(0xff27AE60);
+const kWarningColor = Color(0xffE2B93B);
+const kErrorColor = Color(0xffEB5757);
+// Black Colors
+const kBlack1Color = Color(0xff000000);
+const kBlack2Color = Color(0xff1D1D1D);
+const kBlack3Color = Color(0xff282828);
+// Grey Colors
+const kGray1Color = Color(0xff333333);
+const kGray2Color = Color(0xff4F4F4F);
+const kGray3Color = Color(0xff828282);
+const kGray4Color = Color(0xffBDBDBD);
+const kGray5Color = Color(0xffE0E0E0);
+
+MaterialColor themePrimary = const MaterialColor(0xff092C4C, <int, Color>{
+  50: Color(0xff092C4C),
+  100: Color(0xff092C4C),
+  200: Color(0xff092C4C),
+  300: Color(0xff092C4C),
+  400: Color(0xff092C4C),
+  500: Color(0xff092C4C),
+  600: Color(0xff092C4C),
+  700: Color(0xff092C4C),
+  800: Color(0xff092C4C),
+  900: Color(0xff092C4C),
+},
+);
