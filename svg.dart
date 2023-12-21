69b042c08a8b50fc9727e857448d077e82bf977b
deniz.kavaz <106762076+denizkavaz@users.noreply.github.com>
2023-12-21 18:53:08 +0300

    Create svg.dart

---
+++ svg.dart
@@ -0,0 +1,16 @@
+import 'package:flutter_svg/svg.dart';
+
+// flutter_svg: ^2.0.9
+
+
+SvgPicture.asset(
+  'assets/filter.svg',
+  colorFilter: ColorFilter.mode(Colors.red, BlendMode.srcIn),
+  semanticsLabel: 'Category Filter'
+),
+
+// SVG Options
+// Styling: Presentation Attributes
+// Images: Embed
+// Decimal: 2
+                  
