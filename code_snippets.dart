e68d01f0ed133462190b13b1ad461811216aed7e
deniz.kavaz <106762076+denizkavaz@users.noreply.github.com>
2024-06-22 08:02:37 +0300

    Create code_snippets.dart

---
+++ code_snippets.dart
@@ -0,0 +1,5 @@
+//The Flutter extension includes the following snippets:
+
+//Prefix stless: Create a new subclass of -StatelessWidget`.
+//Prefix stful: Create a new subclass of StatefulWidget and its associated State subclass.
+//Prefix stanim: Create a new subclass of StatefulWidget, and its associated State subclass including a field initialized with an AnimationController.
