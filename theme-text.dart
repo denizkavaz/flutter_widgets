f2b7cd3622ca51d4b2aba9f6759074827b720f1e
deniz.kavaz <106762076+denizkavaz@users.noreply.github.com>
2024-08-19 14:01:41 +0300

    Create theme-text.dart

---
+++ theme-text.dart
@@ -0,0 +1,8 @@
+Text(
+                      'metin buraya',
+                      style: Theme.of(context).textTheme.bodyLarge?.copyWith(
+                            color: Colors.blue, // Override color
+                            fontWeight: FontWeight.bold, // Override font weight
+                            letterSpacing: 2.0, // Add letter spacing
+                          ),
+                    ),
